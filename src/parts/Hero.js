4f23c3d170b964e59c2ae44b064ce30545db69eb
MaulanaAdil <kenari222@gmail.com>
2021-09-09 19:57:35 +0700

    hero section complete

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -7,6 +7,7 @@ import IconTraveler from "assets/icons/icons-traveler.svg";
 import IconTreasure from "assets/icons/icons-treasure.svg";
 
 import Button from "elements/Button";
+import formatNumber from "utils/formatNumber";
 
 export default function Hero(props) {
   function showMostPicked() {
@@ -20,12 +21,15 @@ export default function Hero(props) {
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work,
             <br />
             Start Next Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p
+            className="mb-4 font-weight-light text-gray-500 w-75"
+            style={{ lineHeight: "170%" }}
+          >
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments.
           </p>
@@ -37,8 +41,8 @@ export default function Hero(props) {
           >
             Show Me Now
           </Button>
-          <div className="row mt-5">
-            <div className="col-auto text-center" style={{ marginRight: 30 }}>
+          <div className="row" style={{ marginTop: 80 }}>
+            <div className="col-auto text-center" style={{ marginRight: 25 }}>
               <img
                 width="36"
                 height="36"
@@ -46,13 +50,13 @@ export default function Hero(props) {
                 alt={`${props.data.travelers} Travelers`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
+                {formatNumber(props.data.travelers)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   Travelers
                 </span>
               </h6>
             </div>
-            <div className="col-auto text-center" style={{ marginRight: 30 }}>
+            <div className="col-auto text-center" style={{ marginRight: 25 }}>
               <img
                 width="36"
                 height="36"
@@ -60,13 +64,13 @@ export default function Hero(props) {
                 alt={`${props.data.treasures} Treasure`}
               />
               <h6 className="mt-3">
-                {props.data.treasures}{" "}
+                {formatNumber(props.data.treasures)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   Treasure
                 </span>
               </h6>
             </div>
-            <div className="col-auto text-center" style={{ marginRight: 30 }}>
+            <div className="col-auto text-center" style={{ marginRight: 25 }}>
               <img
                 width="36"
                 height="36"
@@ -74,7 +78,7 @@ export default function Hero(props) {
                 alt={`${props.data.cities} Cities`}
               />
               <h6 className="mt-3">
-                {props.data.cities}{" "}
+                {formatNumber(props.data.cities)}{" "}
                 <span className="text-gray-500 font-weight-light">Cities</span>
               </h6>
             </div>
@@ -85,13 +89,13 @@ export default function Hero(props) {
           <div className="image-hero">
             <img
               src={ImageHero}
-              alt="Image hero"
+              alt="pic hero"
               className="img-fluid position-absolute"
               style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
             />
             <img
               src={ImageHeroFrame}
-              alt="Image hero frame"
+              alt="pic hero frame"
               className="img-fluid position-absolute"
               style={{ margin: "0 15px -15px 0" }}
             />
