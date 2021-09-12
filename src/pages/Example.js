c810248ecb0dddb16ac7d19d2777fc69d0838219
MaulanaAdil <kenari222@gmail.com>
2021-09-12 21:53:15 +0700

    Add element Breadcrumb completed

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,18 +1,24 @@
+import Breadcrumb from "elements/Breadcrumb";
 import React, { Component } from "react";
 
 export default class Example extends Component {
-  state = {
-    value: {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  };
+  // state = {
+  //   value: {
+  //     startDate: new Date(),
+  //     endDate: new Date(),
+  //     key: "selection",
+  //   },
+  // };
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  // handleChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
   render() {
+    const breadcrumbList = [
+      { pageTitle: "Home", pageHref: "/" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
+
     return (
       <div className="container">
         <div
@@ -21,6 +27,7 @@ export default class Example extends Component {
         >
           <div className="col-auto">
             {/* Isi Component yang mau dilihat sesudah ditest */}
+            <Breadcrumb data={breadcrumbList} />
           </div>
         </div>
       </div>
