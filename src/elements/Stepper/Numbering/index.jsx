2d79a830ffda0b448145947d2ab123b5b205395b
MaulanaAdil <kenari222@gmail.com>
2021-09-13 20:36:37 +0700

    change .js to .jsx

---
+++ src/elements/Stepper/Numbering/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Numbering(props) {
+  return <div></div>;
+}
---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "parts/Header";
+
+export default function CheckoutPage() {
+  return (
+    <>
+      <Header isCentered />
+    </>
+  );
+}
